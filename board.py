003a490de4bccb0959cca11f6b1a3bec414e190b
benjhatch <benjamin.hat4@gmail.com>
2019-02-05 23:03:54 -0800

    trying to add flexibility to window size

--- board.py
+++ board.py
@@ -5,24 +5,26 @@ from snake_nest import SnakeNest
 import pygame as pg
 
 class Board:
-    def __init__(self,rows=0, cols=0, numSnakes=1, lengthSnake=3, apples=1):
+    def __init__(self,rows=0, cols=0, size = 50, numSnakes=1, lengthSnake=3, apples=1):
         self.rows = rows
         self.cols = cols
+        self.size = size
         self.apples = AppleTree(apples, rows, cols)
         self.snakes = SnakeNest(numSnakes, lengthSnake, rows, cols, self.apples)
 
 
     def displayGame(self,screen):
         screen.fill((0,0,0))
+        size = self.size
         for apple in self.apples.appleTree:
-            pg.draw.rect(screen, (255, 0, 0), ((apple.loc[1] * 50) + 1, (apple.loc[0] * 50) + 1, 48, 48))
+            pg.draw.rect(screen, (255, 0, 0), ((apple.loc[1] * size) + 1, (apple.loc[0] * size) + 1, size-2, size-2))
         for i in range(len(self.snakes.nest)):
             for segment in self.snakes.nest[i].body:
                 if i == 0:
                     color = (0, 255, 0)
                 else:
                     color = (0, 0, 255)
-                pg.draw.rect(screen, color, ((segment.loc[1] * 50) + 1, (segment.loc[0] * 50) + 1, 48, 48))
+                pg.draw.rect(screen, color, ((segment.loc[1] * size) + 1, (segment.loc[0] * size) + 1, size-2, size-2))
         pg.display.update()
 
     def determineSnake(self,key): #still needs work
