4a3ffdd0f6e094274545dce2eb192d973613bf66
benjhatch <benjamin.hat4@gmail.com>
2019-02-27 13:29:00 -0800

    this is now the original, non online snake game...working on online version in another file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from jungle import Jungle
-from server import Server
 import pygame as pg
 import time
 
@@ -10,22 +9,17 @@ keyHit = False
 #change me...use wasd for first snake
 #rows, cols, size, number of snakes, length of snakes, number of apples, number added on contact with apple
 jungle = Jungle(30, 30, 20, 2, 5, 3, 5)
-server = Server(jungle.snakes, len(jungle.snakes))
 pg.init()
 
 while run:
-    server.pingClients()
     jungle.screen.fill((0, 0, 0))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+        if event.type == pg.KEYDOWN and event.key in jungle.keys:
+            tuple = jungle.keys[event.key]
+            snake = jungle.snakes[tuple[0]]
+            snake.changeDir(tuple[1])
     jungle.moveSnakes()
     pg.display.update()
     time.sleep(timeLimit / 5000)
-
-"""
-    if event.type == pg.KEYDOWN and event.key in jungle.keys:
-        tuple = jungle.keys[event.key]
-        snake = jungle.snakes[tuple[0]]
-        snake.changeDir(tuple[1])
-"""
